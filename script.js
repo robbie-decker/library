2eb688cbfd2bdc651f0ea50e50bec05eccbccf56
Robbie Decker <robbd56@gmail.com>
2023-08-06 12:03:59 -0400

    Implements add book, remove book, and read status

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
 let myLibrary = [];
+const shelf = document.getElementById("books");
 
 function Book(title, author, pages){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.readStatus = false;
+}
+
+Book.prototype.changeReadStatus = function(){
+    this.readStatus = !this.readStatus;
+    console.log(this.readStatus);
 }
 
 function addBookToLibary(title, author, pages){
     newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
+    displayBooks();
 }
 
-function createCard(book){
-    const shelf = document.getElementById("books");
-    const bookInfo = document.createElement('div');
+function createCard(book, i){
+    const bookCard = document.createElement('div'); 
     const authorInfo = document.createElement('p');
     const titleInfo = document.createElement('p');
     const pagesInfo = document.createElement('p');
+    const removeBook = document.createElement('button');
+    const readStatus = document.createElement('input');
+    readStatus.setAttribute('type', 'checkbox');
+    // bookCard.dataset.index = i;
+
+    removeBook.addEventListener('click', () => {
+        myLibrary.splice(i, 1);
+        displayBooks();
+    });
+
+    readStatus.addEventListener('change', () =>{
+        book.changeReadStatus();
+    });
 
     titleInfo.textContent = book.title;
     authorInfo.textContent = book.author;
     pagesInfo.textContent = book.pages;
-    bookInfo.append(titleInfo, authorInfo, pagesInfo);
-    shelf.appendChild(bookInfo);
+    removeBook.textContent = "REMOVE";
+    bookCard.classList.add("card");
+
+    bookCard.append(titleInfo, authorInfo, pagesInfo, removeBook, readStatus);
+    shelf.appendChild(bookCard);
 }
 
 function displayBooks(){
-    for(book of myLibrary){
-        console.log(book);
-        createCard(book);
+    shelf.replaceChildren();
+    for(const [i, book] of myLibrary.entries()){
+        createCard(book, i);
     }
 }
 
@@ -44,4 +67,3 @@ bookButton.addEventListener('click', () =>{
 
 addBookToLibary("Romeo and Juliet", "William Shakespear", 225);
 addBookToLibary("Player Piano", "Kurt Vonnegut", 336);
-displayBooks();
